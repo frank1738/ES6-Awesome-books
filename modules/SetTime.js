798a0ab8a1449563c586b1b8e537e4f23f043679
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-25 20:29:41 +0300

    fix linter errors

--- modules/SetTime.js
+++
@@ -1,7 +0,0 @@
-import { DateTime } from './luxon/time.js';
-import { siteDate } from '../index.js';
-export const currentTime = () => {
-  const theTime = DateTime.now();
-  console.log(theTime);
-  siteDate.innerHTML = `${theTime.c.day}/${theTime.c.month}/${theTime.c.year}`;
-};
